61bffd083339d98a2ab219d934692ae2a21ecaf4
Parth Kabade <151639939+qwejead@users.noreply.github.com>
2026-05-16 01:47:24 +0530

    Add files via upload

---
+++ SingleLevelProtected.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+class Base
+{
+protected:
+    int i, j;
+
+public:
+    Base()
+    {
+        cout << "Inside base constructor\n";
+        i = 0;
+        j = 0;
+    }
+
+    ~Base()
+    {
+        cout << "Inside Base distructor\n";
+    }
+
+    void fun()
+    {
+        cout << "Inside Base Fun\n";
+    }
+};
+
+class Derived : public Base
+{
+public:
+    int x;
+
+    Derived()
+    {
+        cout << "Inside Derived constructor\n";
+        x = 0;
+    }
+
+    ~Derived()
+    {
+        cout << "Inside Derived Distructor\n";
+    }
+
+    void gun()
+    {
+        cout << "Inside gun of derived\n";
+    }
+};
+
+int main()
+{
+    cout << "Inside main\n";
+
+    Derived dobj;
+
+    cout << "Size of Base class objest is:" << sizeof(Base) << "\n"; // 8
+
+    cout << "Size of Derived class objest is:" << sizeof(Derived) << "\n"; // 12
+
+    // cout << dobj.i << "\n"; // error
+    // cout << dobj.j << "\n"; // error
+    cout << dobj.x << "\n"; // 0
+
+
+    cout << "End of main\n";
+
+    return 0;
+}
